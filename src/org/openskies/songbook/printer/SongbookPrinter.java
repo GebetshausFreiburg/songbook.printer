dcf3d5540207cb845231a95ab6054238a0106c89
Matthias Wegner <matthias.wegner@gebetshaus-freiburg.de>
2019-03-08 15:04:54 +0100

    Added Plain-Print

--- src/org/openskies/songbook/printer/SongbookPrinter.java
+++ src/org/openskies/songbook/printer/SongbookPrinter.java
@@ -27,7 +27,7 @@ public class SongbookPrinter {
 	final static Logger LOGGER = LogManager.getLogger(SongbookPrinter.class);
 
 	/**
-	 * The main method.
+	 * The main method of the songbook-creator
 	 *
 	 * @param args the arguments
 	 */
